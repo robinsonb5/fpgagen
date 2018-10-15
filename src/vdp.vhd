7a2b9ee1029253b7276d021700fcd4f0cf61f5f8
Gyorgy Szombathelyi <gyurco@freemail.hu>
2018-10-15 14:03:53 +0200

    Fix window base calc in H40 mode
    LSB of NTWB must be ignored in H40

--- src/vdp.vhd
+++ src/vdp.vhd
@@ -1374,7 +1374,7 @@ begin
 				
 			when BGAC_CALC_BASE =>
 				if WIN_H = '1' or WIN_V = '1' then
-					V_BGA_XBASE := (NTWB & "00000000000") + (BGA_POS(9 downto 3) & "0");
+					V_BGA_XBASE := (NTWB(4 downto 1) & (not H40 and NTWB(0))  & "00000000000") + (BGA_POS(9 downto 3) & "0");
 					if H40 = '0' then -- WIN is 32 tiles wide in H32 mode
 						V_BGA_BASE := V_BGA_XBASE + (BGA_Y(9 downto 3) & "00000" & "0");
 					else              -- WIN is 64 tiles wide in H40 mode
